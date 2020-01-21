2a260b54679d0c30ef6704cb0f74e852d211c360
Rachael Rice <Rachnicrice@gmail.com>
2020-01-21 10:15:19 -0800

    Added /hello and /capitalize routes as well as all-caps.html template

--- src/main/java/com/rachnicrice/songr/SongrController.java
+++ src/main/java/com/rachnicrice/songr/SongrController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SongrController {
 
+
+    @GetMapping ("/")
+    public String home (Model m) {
+        return "home";
+    }
+
     @GetMapping("/hello")
     public String sayHello (Model m) {
-
         return "hello";
+    }
+
+    @GetMapping("/capitalize")
+    public String capitalize (Model m) {
+        return "all-caps";
     }
 }
