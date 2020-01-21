5bd6768f945594a49d69f7fa7e380d0ce5f6e145
Rachael Rice <Rachnicrice@gmail.com>
2020-01-21 11:11:03 -0800

    added display for hardcoded albums

--- src/main/java/com/rachnicrice/songr/SongrController.java
+++ src/main/java/com/rachnicrice/songr/SongrController.java
@@ -17,7 +17,12 @@ public class SongrController {
 
     @GetMapping("/albums")
     public String albums (Model m) {
-
+        Album[] albums = new Album[] {
+            new Album("Fear Inoculum", "Tool", 10, 5198, "https://en.wikipedia.org/wiki/Fear_Inoculum#/media/File:Tool_-_Fear_Inoculum.png"),
+            new Album ("Stromata", "Charlotte Martin", 12, 3284, "I'll save images to assets eventually"),
+            new Album ("Rumors", "Fleetwood Mac", 11, 2383, "This is a great album")
+        };
+        m.addAttribute("albums", albums);
         return "albums";
     }
 
